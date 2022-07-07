dd0e75c9d81c0c3883f73ac4f6e9404cc190768e
tallesChristian <72113346+tallesChristian@users.noreply.github.com>
2022-07-06 21:12:31 -0300

    Adicionar arquivos de projeto.

---
+++ APIShapeSkate/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APIShapeSkate.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions <DataContext> opt) 
+            : base(opt)
+        {
+
+        }
+
+        public DbSet<Shape> Shapes { get; set; }
+
+
+    }
+}
---
+++ APIShapeSkate/Models/Shape.cs
@@ -0,0 +1,10 @@
+﻿namespace APIShapeSkate.Data
+{
+    public class Shape
+    {
+        public int Id { get; set; }
+        public string Madeira { get; set; }
+        public int Tamanho { get; set; }
+        public float Valor { get; set; }
+    }
+}
